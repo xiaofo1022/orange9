8cd51109e9b8cf3305850c33eb0850e07913730e
Kurt Yu <kurt.yu@perficient.com>
2015-03-27 15:46:01 +0800

    Today is kind of shit.

---
+++ js/tripper.js
@@ -0,0 +1,33 @@
+﻿var Tripper = function(id, tripperText, tripperClass, hoverId) {
+	this.component = $("#" + id);
+	this.hover = hoverId ? $("#" + hoverId) : this.component;
+	this.defaultText = this.component.text();
+	this.tripperText = tripperText;
+	this.over = false;
+	
+	var ins = this;
+	this.hover.bind("transitionend", function() {
+		ins.component.css("transform", "rotateY(0deg)");
+		if (ins.over) {
+			ins.component.text(ins.tripperText);
+			if (tripperClass) {	
+				ins.component.addClass(tripperClass);
+			}
+		} else {
+			ins.component.text(ins.defaultText);
+			if (tripperClass) {	
+				ins.component.removeClass(tripperClass);
+			}
+		}
+	});
+	
+	this.hover.bind("mouseover", function() {
+		ins.over = true;
+		ins.component.css("transform", "rotateY(90deg)");
+	});
+	
+	this.hover.bind("mouseout", function() {
+		ins.over = false;
+		ins.component.css("transform", "rotateY(90deg)");
+	});
+};
