f00b478a4111f8a9d55dbb11839e93b11f39beee
Little Buddha <553216263@qq.com>
2015-04-06 00:58:49 +0800

    New blood to the system.

---
+++ js/progress/processer.js
@@ -0,0 +1,14 @@
+﻿var Processer = function(title, bar) {
+	this.title = title;
+	this.bar = bar;
+};
+
+Processer.prototype = {
+	run: function(num, count) {
+		var controlTitle = $('#' + this.title).text('本月进度（共' + count + '单）');
+		var controlBar = $('#' + this.bar).NumberProgressBar({
+		  duration: 12000,
+		  current: num
+		});
+	}
+};
